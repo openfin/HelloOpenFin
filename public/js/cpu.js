9730319539db50e1cf85736f6553e89f270e5008
datamadic <xavier@openfin.co>
2014-05-20 12:13:20 -0400

    cleaning up the cpu stuff

--- public/js/cpu.js
+++ public/js/cpu.js
@@ -6,11 +6,20 @@ var cpu = cpu || {};
     function genConfig() {
         var rand = Math.random();
         return {
-            name: "cpuChild" + rand,
-            defaultWidth: 960,
-            defaultHeight: 390,
-            autoShow: true,
-            url: 'views/cpu.html'
+            "name": "cpuChild" + (rand * 100) % 40 ,
+            "defaultWidth": 960,
+            "defaultHeight": 390,
+            "autoShow": true,
+            "url": 'views/cpu.html',
+
+            "cornerRounding" : {
+                "width" : 5,
+                "height" : 5
+            },
+            "frame" : false,
+            "resizable": false
+
+
         };
     }
 
