df2ddda4b9400d9cd2704db063137afb395c3553
Ricardo de pena <ricardo.depena@gmail.com>
2014-05-22 12:05:12 -0400

    consolidated the transparency and animation, removed logic from the OpenFin Main function.

--- public/js/cpu-window.js
+++ public/js/cpu-window.js
@@ -3,11 +3,11 @@ var cpuChart = cpuChart || {};
 console.log('this is my cpuChart obj', cpuChart);
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    //OpenFin is ready.
     fin.desktop.main(function() {
 
-        var ofDraggable = document.querySelectorAll('.of-draggable')[0],
-            closeButton = document.querySelectorAll('#close-app')[0],
-
+        var closeButton = document.querySelectorAll('#close-app')[0],
             mainWindow = fin.desktop.Window.getCurrent();
 
         /* kick off a d3 chart that displays cpu usage data */
@@ -18,24 +18,8 @@ document.addEventListener('DOMContentLoaded', function() {
             mainWindow.hide();
         });
 
-        utils.registerDragHandler(ofDraggable, {
-            //once movement starts make the window transparent.
-            onDragStart: function(x, y) {
-                mainWindow.updateOptions({
-                    opacity: 0.5
-                });
-            },
-            //move the window with the mouse
-            onDrag: function(X, Y) {
-                window.moveTo(X, Y);
-            },
-            //window can now stop being transparent.
-            onDragEnd: function() {
-                mainWindow.updateOptions({
-                    opacity: 1
-                });
-            }
-        });
+        //set up window move effects.
+        utils.registerDragHandler(mainWindow);
 
     });
 });
--- public/js/utils.js
+++ public/js/utils.js
@@ -1,53 +1,70 @@
+var utils = utils || {};
 (function() {
     'use strict';
-    var utils = {};
+
+    var transitionDuration = 200,
+        transparentOpacityVal = 0.7;
+
+    //transparency animation used.
+    utils.transparentOpacityAnimation = {
+        opacity: transparentOpacityVal,
+        //time in ms
+        duration: transitionDuration
+    };
+
+    //solid animation options used.
+    utils.solidOpacityAnimation = {
+        opacity: 1,
+        //time in ms.
+        duration: transitionDuration
+    };
+
     //will make an object dragable.
-    utils.registerDragHandler = function registerDragHandler(dragableElement, options) {
+    utils.registerDragHandler = function registerDragHandler(mainWindow) {
         var mouseDown = false,
             mouseMoved = false,
+            threshold = 3,
             offset = {},
+            ofDraggable = document.querySelectorAll('.of-draggable')[0],
             isFunction = function(val) {
                 return typeof val === 'function';
             },
             noop = function() {};
 
-        options.threshold = options.threshold || 3;
-        options.onDrag = options.onDrag || noop;
-        options.onDragStart = options.onDragStart || noop;
-        options.onDragEnd = options.onDragEnd || noop;
-
         //obtain the initial mouse position and initiate the tracking.
-        dragableElement.addEventListener('mousedown', function(e) {
+        ofDraggable.addEventListener('mousedown', function(e) {
             mouseDown = true;
             offset.x = e.x;
             offset.y = e.y;
         });
 
         //obtain the mouse position via the OpenFin API and execute the callbacks.
-        dragableElement.addEventListener('mousemove', function(e) {
+        ofDraggable.addEventListener('mousemove', function(e) {
             if (mouseDown) {
                 //is the movement within the threshold ?
-                if (!mouseMoved && Math.sqrt(Math.pow((e.x - offset.x), 2) + Math.pow((e.y - offset.y), 2)) >= options.threshold) {
+                if (!mouseMoved && Math.sqrt(Math.pow((e.x - offset.x), 2) + Math.pow((e.y - offset.y), 2)) >= threshold) {
                     mouseMoved = true;
-                    fin.desktop.System.getMousePosition(function(data) {
-                        options.onDragStart(data.left - offset.x, data.top - offset.y);
+
+                    //animate effect.
+                    mainWindow.animate({
+                        opacity: utils.transparentOpacityAnimation
                     });
                 }
 
                 //get the mouse position via the api for all subsequent move events.
                 if (mouseMoved) {
                     fin.desktop.System.getMousePosition(function(data) {
-                        options.onDrag(data.left - offset.x, data.top - offset.y);
+                        mainWindow.moveTo(data.left - offset.x, data.top - offset.y);
                     });
                 }
             }
         });
 
         //if movement was being tracked call the end.
-        dragableElement.addEventListener('mouseup', function(e) {
+        ofDraggable.addEventListener('mouseup', function(e) {
             if (mouseMoved && e.button === 0) {
-                fin.desktop.System.getMousePosition(function(data) {
-                    options.onDragEnd(data.left - offset.x, data.top - offset.y);
+                mainWindow.animate({
+                    opacity: utils.solidOpacityAnimation
                 });
             }
 
@@ -55,7 +72,4 @@
             mouseMoved = false;
         });
     };
-
-    //set utils as a global variabe.
-    window.utils = utils;
 }());
